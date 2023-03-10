e62cf1208ad262969fce7f088ac1d630b420b35a
deepshikha jangid <Deepshikha.jangid@cars24.com>
2023-03-10 23:34:18 +0530

    Initial commit

---
+++ app/src/main/java/deepshikha/jangidyahoo/finalyearproject/MainActivity.java
@@ -0,0 +1,20 @@
+package deepshikha.jangidyahoo.finalyearproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
