46fa0f08f4724889e8e40309aa4bf0018c1405af
deepshikha jangid <Deepshikha.jangid@cars24.com>
2023-03-21 14:46:26 +0530

    added Text to speech function

--- app/src/main/java/deepshikha/jangidyahoo/finalyearproject/MainActivity.java
+++ app/src/main/java/deepshikha/jangidyahoo/finalyearproject/MainActivity.java
@@ -8,13 +8,38 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
+    TextToSpeech textToSpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                // if No error is found then only it will run
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                    textToSpeech.setSpeechRate(1);
+                }
+            }
+        });
+    }
+
+    public void TextBoxCLickMethod(View view) {
+        view.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak("You Are Clicking" + view.getContentDescription().toString(),TextToSpeech.QUEUE_FLUSH,null);            }
+        });
     }
 }
