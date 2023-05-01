0aba80d1ed8783bd73903e449795d7f4deda7f57
deepshikha jangid <deepshikha.jangid@yahoo.com>
2023-05-01 23:43:44 +0530

    optimized previous code for better performance and made the layout responsive

--- app/src/main/java/deepshikha/jangidyahoo/finalyearproject/MainActivity.java
+++ app/src/main/java/deepshikha/jangidyahoo/finalyearproject/MainActivity.java
@@ -1,45 +1,72 @@
 package deepshikha.jangidyahoo.finalyearproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+import deepshikha.jangidyahoo.finalyearproject.Adapter.HomeGridViewAdapter;
+import deepshikha.jangidyahoo.finalyearproject.model.HomeGridViewModel;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener  {
     TextToSpeech textToSpeech;
+    AudioManager audioManager;
+    GridView homeGV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
-        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
 
-                // if No error is found then only it will run
-                if(i!=TextToSpeech.ERROR){
-                    // To Choose language of speech
-                    textToSpeech.setLanguage(Locale.ENGLISH);
-                    textToSpeech.setSpeechRate(1);
-                }
-            }
-        });
+        // Adding Grid Elements
+        homeGV = findViewById(R.id.homeGridView);
+        ArrayList<HomeGridViewModel> ModelArrayList = new ArrayList<HomeGridViewModel>();
+        ModelArrayList.add(new HomeGridViewModel("Messages", R.drawable.ic_baseline_message_24));
+        ModelArrayList.add(new HomeGridViewModel("Phone", R.drawable.ic_baseline_add_ic_call_24));
+        ModelArrayList.add(new HomeGridViewModel("Notes", R.drawable.ic_baseline_event_note_24));
+        ModelArrayList.add(new HomeGridViewModel("Battery", R.drawable.ic_baseline_battery_charging_full_24));
+        HomeGridViewAdapter adapter = new HomeGridViewAdapter(this, ModelArrayList);
+        homeGV.setAdapter(adapter);
+
+        // Introductory Text
+        textToSpeech = new TextToSpeech(this,this);
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                .build();
+        textToSpeech.setAudioAttributes(audioAttributes);
+        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
     }
 
-    public void TextBoxCLickMethod(View view) {
-        view.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
 
-            @Override
-            public void onClick(View view) {
-                textToSpeech.speak("You Are Clicking" + view.getContentDescription().toString(),TextToSpeech.QUEUE_FLUSH,null);            }
-        });
+            int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
+            // Text-to-speech is ready to use
+            String introText = "Welcome to voice assistant app. Click on the different sides of screen to know details" ;
+            textToSpeech.speak(introText, TextToSpeech.QUEUE_FLUSH, null);
+
+        }
     }
 }
